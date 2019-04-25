ca0f2f9e6ad7bdf261e285f819dbfa6a4d3a3565
Bidur Adhikari <badhikari@godaddy.com>
2019-04-25 13:58:55 -0700

    add chrome dev tool support

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,9 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
-export default createStore(rootReducer);
+/* eslint-disable no-underscore-dangle */
+export default createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+/* eslint-enable */
